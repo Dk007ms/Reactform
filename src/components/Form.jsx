cf03f7f5790ff659cdf24e8fb7ae201caf20b4ae
Dk007ms <dkaushik290@gmail.com>
2024-01-19 17:37:16 +0530

    Form initialised

---
+++ src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Form() {
+  return (
+    <div className='w-11/12 h-64' >
+       <label htmlFor='FirstName'>First Name</label>
+      <input id='FirstName' type='text' className=" border-2 border-black" />
+      
+    </div>
+  )
+}
